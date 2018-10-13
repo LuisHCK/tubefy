461098fdef1192b879c71b4ce064e39a2cf94068
Luis J. Centeno <luisjcenteno@live.com>
2018-10-12 22:42:18 -0600

    start over

--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie';
-
-const db = new Dexie('wrkstation');
-db.version(1).stores({
-    songs: `++id, name, url, favorite`,
-    playlists: '++id, title, cover, *songs'
-});
-
-export default db;
